edd79d517ec2a85fc7393f2ae2c8e0c3c53de018
Marcos Navarro <marcos@aftertype.com>
2016-11-25 19:23:15 -0700

    First Go file with comments

---
+++ first-go-file.go
@@ -0,0 +1,22 @@
+package main
+
+// This is a comment
+
+/*
+	Another comment
+*/
+
+// Import format package
+import "fmt"
+
+/*
+	Available Operations
+	+ - * / %
+*/
+
+func main(){
+
+	// format print line
+	fmt.Println("Hello World")
+	
+}
